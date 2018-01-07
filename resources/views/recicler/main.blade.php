bc51d2c371e3037d932faa02dd80a772425f5aca
Rakzodia <jeec.estaba@gmail.com>
2018-01-07 17:41:46 +0000

    [add] Font Awesome

--- resources/views/recicler/main.blade.php
+++ resources/views/recicler/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link type="text/css" rel="stylesheet" href="{{assets('vendors/materialize/css/materialize.min.css')}}"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="{{assets('css/regular.css')}}"/>
-
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" />
 </head>
 <body>
 	<nav class="yellow black-text">
